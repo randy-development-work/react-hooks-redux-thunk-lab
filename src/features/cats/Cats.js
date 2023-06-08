4ad9a7522a5ca6ba06c15ec891bdcc27dd0f1d75
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-06-08 12:24:42 +0300

    Add a spinner to indicate Loading before the images have been rendered.

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -6,6 +6,7 @@ import CatList from "./CatList";
 function Cats() {
   const catPics = useSelector((state) => state.entities);
   const dispatch = useDispatch();
+  const status = useSelector((state) => state.status);
 
   useEffect(() => {
     dispatch(fetchCats());
@@ -14,6 +15,13 @@ function Cats() {
   return (
     <div>
       <h1>CatBook</h1>
+      {status === "loading" ? (
+        <div className="d-flex justify-content-center">
+        <div className="spinner-grow text-danger" style={{width: "10rem", height: "10rem"}} role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        </div>
+      ) : null}
       <CatList catPics={catPics} />
     </div>
   );
