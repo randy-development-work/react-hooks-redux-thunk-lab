81073285366e77e0fe80f8c99c74ac8d14e0201c
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-06-08 11:26:10 +0300

    1. Incorporate thunk middleware to the program. 2. Have useEffect to fetch the cat data once the component renders. 3. Create actions, reducers and fetch in the Slice file.

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCats } from "./catsSlice";
 
 function Cats() {
+  const catPics = useSelector((state) => state.entities);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, []);
+
+  console.log(catPics);
   return (
     <div>
       <h1>CatBook</h1>
